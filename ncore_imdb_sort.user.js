e8bcc8048cb433925b8aebdd4b308ffd73e11bdb
David Elek <elekdavidistvan@gmail.com>
2016-11-21 12:55:33 +0100

    Chnaging localStorage to Tampermonkey storage in imdb sorter

--- ncore_imdb_sort.user.js
+++ ncore_imdb_sort.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         nCore Sort torrents by iMDB rating
 // @namespace    https://github.com/elekdavid/tampermonkey-scripts
+// @source       https://github.com/elekdavid/tampermonkey-scripts/raw/master/ncore_imdb_sort.user.js
 // @version      0.1
 // @description  Adding an option to sort the torrents on nCore by iMDB rating. Only works on one page, recommended to put the page size to 100 in the nCore settings.
 // @author       David Elek
 // @match        ncore.cc/torrents.php*
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @require http://code.jquery.com/jquery-latest.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/tinysort/2.3.6/tinysort.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/tinysort/2.3.6/jquery.tinysort.min.js
@@ -20,8 +22,8 @@
     // Checkbox html
     const checkboxHtml =
           "<div style='display: block; text-align: center'>" +
-          "<label><input type='checkbox' id='imdb_sort_checkbox'>" +
-          "<label for='imdb_sort_checkbox'> Enable IMDB sort</label>" +
+          "  <input type='checkbox' id='imdb_sort_checkbox'>" +
+          "  <label for='imdb_sort_checkbox'> Enable IMDB sort</label>" +
           "</div>" +
           "<div class='hr_stuff' deluminate_imagetype='gif'></div>";
 
@@ -61,22 +63,17 @@
     function addSortingCheckbox() {
         $("#torrents1 > div.fobox_tartalom").prepend(checkboxHtml);
         var checkbox = $("#imdb_sort_checkbox");
-        if (typeof(Storage) !== "undefined" && localStorage.getItem(storageKey)) {
+        if (GM_getValue(storageKey) === "true") {
             checkbox.attr("checked", true);
             $(".box_torrent_all>div").tsort({attr:"data-imdb", order:"desc"});
         }
         checkbox.change(function(){
             if(this.checked) {
-                if (typeof(Storage) !== "undefined") {
-                    localStorage.setItem(storageKey, true);
-                    $(".box_torrent_all>div").tsort({attr:"data-imdb", order:"desc"});
-                }
-                sortTorrents();
+                GM_setValue(storageKey, "true");
+                $(".box_torrent_all>div").tsort({attr:"data-imdb", order:"desc"});
             } else {
-                if (typeof(Storage) !== "undefined") {
-                    localStorage.removeItem(storageKey);
-                    $(".box_torrent_all>div").tsort({attr:"data-original-order"});
-                }
+                GM_setValue(storageKey, "false");
+                $(".box_torrent_all>div").tsort({attr:"data-original-order"});
             }
         });
     }
